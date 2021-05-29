a234a32b13993520343977b30b7da75902fa79e6
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-05-29 19:05:01 +0200

    added theme data

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
       title: 'Flutter App',
       home: MyHomePage(),
     );
